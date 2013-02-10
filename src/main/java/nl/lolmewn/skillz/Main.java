1020d1f4b2eee06bba83f48d0259aabb19c5ada0
Lolmewn <Lolmewn@SybrensPC>
2013-02-10 19:22:45 +0100

    Restart

--- src/main/java/nl/lolmewn/skillz/Main.java
+++ src/main/java/nl/lolmewn/skillz/Main.java
@@ -1,44 +1,17 @@
+/*
+ *  Copyright 2013 Lolmewn <info@lolmewn.nl>.
+ */
+
 package nl.lolmewn.skillz;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  *
- * @author Lolmewn
+ * @author Lolmewn <info@lolmewn.nl>
  */
 public class Main extends JavaPlugin{
+
     
-    private Settings settings;
-    private UserManager userManager;
-    
-    protected double newVersion = 0;
-    
-    @Override
-    public void onEnable(){
-        this.settings = new Settings(this);
-        this.userManager = new UserManager(this);
-    }
-    
-    @Override
-    public void onDisable(){
-        this.getUserManager().saveAllUsers();
-        if(this.newVersion != 0){
-            //Set new version in the config
-            this.getConfig().set("version", this.newVersion);
-            this.saveConfig();
-        }
-    }
-    
-    public Settings getSettings(){
-        return this.settings;
-    }
-    
-    public UserManager getUserManager(){
-        return this.userManager;
-    }
-    
-    public double getVersion(){
-        return this.getSettings().getVersion();
-    }
-    
+
 }
